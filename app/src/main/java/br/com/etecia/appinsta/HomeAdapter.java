b429a72528d3bc321a571c9b81c08f2cec971a60
lndsilva <lndsilva@hotmail.com>
2023-10-31 20:16:01 -0300

    criando listagem pagina home

--- app/src/main/java/br/com/etecia/appinsta/HomeAdapter.java
+++ app/src/main/java/br/com/etecia/appinsta/HomeAdapter.java
@@ -4,24 +4,28 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
-public class HomeAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
-    private Context mContexto;
-    private List<HomeContents> lstHomeContents;
+public class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.ViewHolder> {
 
-    public HomeAdapter(Context mContexto, List<HomeContents> lstHomeContents) {
+    private Context mContexto;
+    private List<HomeContents> lstHomeContent;
+
+    public HomeAdapter(Context mContexto, List<HomeContents> lstHomeContent) {
         this.mContexto = mContexto;
-        this.lstHomeContents = lstHomeContents;
+        this.lstHomeContent = lstHomeContent;
     }
 
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view;
 
         LayoutInflater inflater = LayoutInflater.from(mContexto);
@@ -31,19 +35,31 @@ public class HomeAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.idTituloHome.setText(lstHomeContent.get(position).getTitulo());
+        holder.idSubTituloHome.setText(lstHomeContent.get(position).getSubtitulo());
+        holder.idImagemHome.setImageResource(lstHomeContent.get(position).getImagem());
 
     }
 
     @Override
     public int getItemCount() {
-        return lstHomeContents.size();
+        return lstHomeContent.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView idTituloHome;
+        TextView idSubTituloHome;
+        ImageView idImagemHome;
+        CardView idCardHome;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            idTituloHome = itemView.findViewById(R.id.txtTituloHomeProfile);
+            idSubTituloHome = itemView.findViewById(R.id.txtSubHomeProfile);
+            idImagemHome = itemView.findViewById(R.id.imagemHomeProfile);
         }
     }
+
+
 }
--- app/src/main/java/br/com/etecia/appinsta/HomeContents.java
+++ app/src/main/java/br/com/etecia/appinsta/HomeContents.java
@@ -1,4 +1,37 @@
 package br.com.etecia.appinsta;
 
 public class HomeContents {
+    private String titulo;
+    private String subtitulo;
+    private int imagem;
+
+    public HomeContents(String titulo, String subtitulo, int imagem) {
+        this.titulo = titulo;
+        this.subtitulo = subtitulo;
+        this.imagem = imagem;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getSubtitulo() {
+        return subtitulo;
+    }
+
+    public void setSubtitulo(String subtitulo) {
+        this.subtitulo = subtitulo;
+    }
+
+    public int getImagem() {
+        return imagem;
+    }
+
+    public void setImagem(int imagem) {
+        this.imagem = imagem;
+    }
 }
