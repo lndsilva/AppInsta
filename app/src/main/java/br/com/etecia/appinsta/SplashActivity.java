a095da28f28b36c5601be15619daf00013c1e240
lndsilva <lndsilva@hotmail.com>
2023-10-31 10:42:45 -0300

    janela de splash

---
+++ app/src/main/java/br/com/etecia/appinsta/SplashActivity.java
@@ -0,0 +1,27 @@
+package br.com.etecia.appinsta;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_layout);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+
+            }
+        },3000);
+
+
+    }
+}
