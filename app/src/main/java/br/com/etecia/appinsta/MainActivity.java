64bcf27794e230f7070853701ae87ff3d6d74b39
lndsilva <lndsilva@hotmail.com>
2023-10-31 22:17:19 -0300

    interação entre telas

--- app/src/main/java/br/com/etecia/appinsta/MainActivity.java
+++ app/src/main/java/br/com/etecia/appinsta/MainActivity.java
@@ -2,13 +2,26 @@ package br.com.etecia.appinsta;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin = findViewById(R.id.idLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+            }
+        });
     }
 }
